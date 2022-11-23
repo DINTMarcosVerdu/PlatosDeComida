b8c7f2bdb6532853e5897de15f31ecc9b09b4b42
Marcxsvss <90453251+Marcxsvss@users.noreply.github.com>
2022-11-23 12:39:13 +0100

    Agregar archivos de proyecto.

---
+++ PlatosDeComida/MainWindowVm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatosDeComida
+{
+    class MainWindowVm : INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> listaPlatos;
+
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return listaPlatos; }
+            set { listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }
+        private Plato platoSeleccionado;
+
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set
+            {
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+        private ObservableCollection<String> listaPaises;
+        
+
+        public ObservableCollection<String> ListaPaises 
+        {
+            get { return listaPaises; }
+            set
+            {
+                listaPaises = value;
+                NotifyPropertyChanged("ListaPaises");
+            }
+        }
+
+       
+        public MainWindowVm()
+        {
+            listaPlatos = Plato.GetSamples("./assets2/");
+            listaPaises = new ObservableCollection<String>();
+            ListaPaises.Add("China");
+            ListaPaises.Add("Americana");
+            ListaPaises.Add("Mexicana");
+
+        }
+
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PlatosDeComida/classes/Plato.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+
+class Plato : INotifyPropertyChanged
+{
+    public string nombre { get; set; }
+    public string imagen { get; set; }
+    public string tipo { get; set; }
+    public bool gluten { get; set; }
+    public bool soja { get; set; }
+    public bool leche { get; set; }
+    public bool sulfitos { get; set; }
+
+    
+
+    public string Nombre
+    {
+        get { return nombre; }
+        set 
+        { 
+            nombre = value;
+            NotifyPropertyChanged("Nombre");
+        }
+    }
+    public string Imagen
+    {
+        get { return imagen; }
+        set
+        {
+            imagen = value;
+            NotifyPropertyChanged("Imagen");
+        }
+    }
+    public string Tipo
+    {
+        get { return tipo; }
+        set
+        {
+            tipo = value;
+            NotifyPropertyChanged("Tipo");
+        }
+    }
+    public bool Gluten
+    {
+        get { return gluten; }
+        set
+        {
+            gluten = value;
+            NotifyPropertyChanged("Gluten");
+        }
+    }
+    public bool Soja
+    {
+        get { return soja; }
+        set
+        {
+            soja = value;
+            NotifyPropertyChanged("Soja");
+        }
+    }
+    public bool Leche
+    {
+        get { return leche; }
+        set
+        {
+            leche = value;
+            NotifyPropertyChanged("Leche");
+        }
+    }
+    public bool Sulfitos
+    {
+        get { return sulfitos; }
+        set
+        {
+            sulfitos = value;
+            NotifyPropertyChanged("Sulfitos");
+        }
+    }
+
+
+    public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+    {
+        Nombre = nombre;
+        Imagen = imagen;
+        Tipo = tipo;
+        Gluten = gluten;
+        Soja = soja;
+        Leche = leche;
+        Sulfitos = sulfitos;
+    }
+
+    public Plato()
+    {
+    }
+
+    public static ObservableCollection<Plato> GetSamples(string rutaImagenes)
+    {
+        ObservableCollection<Plato> lista = new ObservableCollection<Plato>();
+
+        lista.Add(new Plato("Hamburguesa", Path.Combine(rutaImagenes, @"burguer.jpg"), "Americana", true, false, true, true));
+        lista.Add(new Plato("Dumplings", Path.Combine(rutaImagenes, @"dumplings.jpg"), "China", true, true, false, false));
+        lista.Add(new Plato("Tacos", Path.Combine(rutaImagenes, @"tacos.jpg"), "Mexicana", true, false, false, true));
+        lista.Add(new Plato("Cerdo agridulce", Path.Combine(rutaImagenes, @"cerdoagridulce.jpg"), "China", true, true, false, true));
+        lista.Add(new Plato("Hot dogs", Path.Combine(rutaImagenes, @"hotdog.jpg"), "Americana", true, true, true, true));
+        lista.Add(new Plato("Fajitas", Path.Combine(rutaImagenes, @"fajitas.jpg"), "Mexicana", true, false, false, true));
+
+        return lista;
+    }
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void NotifyPropertyChanged(string propertyName)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
